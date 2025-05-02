8a85e316d11adac5a7a5c254cfd2288069b67b5d
madhi1375afg <mahdiahmadi1996@outlook.de>
2025-05-02 14:38:34 +0200

    new DB im resouce/klausur_app_sql

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,97 +1,33 @@
 package org.example;
-
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 import org.example.dao.DBconn;
-import org.example.domain.*;
 
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.sql.Statement;
+import java.util.Arrays;
 
-public class Main extends Application {
-
+public class Main {
     public static void main(String[] args) throws SQLException {
-        //launch(args);
-        // Hier wird die Datenbankoperation vor dem Starten der JavaFX-Oberfläche ausgeführt
+        String []values = {"netan","drake","gmail@outlook"};
+        String []columns = {"vorname","name","email"};
+
         DBconn db = new DBconn();
 
-        //Fragen hinzufügen
-        benutzerKonto mahdi = new benutzerKonto();
-        /*
-        Modul modul = new Modul(1,"AIN4","allgemein Informatik");
-
-        Fragen frage = new Fragen(2,"wie groß ist eine Elephant", BloomLevel.verstehen,modul,3, FragenArt.GeschlosseneFrage);
-        System.out.println(frage.getFragenArt());
-        mahdi.antwortErstellen(frage);
-        */
-
-        //Fragen bearbeiten
-        //Antwort antwort = new Antwort(2,"Elephant ist 4m groß", true, 1, AntwortType.geschlosseneAntwort);    -----
-         ///mahdi.antwortBearbeiten(antwort);
-
-        //Fragen löschen
-        //mahdi.antwortloeaschen(antwort);      ----
 
 
-
-        // Fehlerbehandlung für Insert
-       /* Object []values= {"aufgabe2","was ist primitiv Datentype",4,"anwenden",1,3,"geschlossene aufgabe"};
-        String []columns = {"name","aufgabetext","zeit","bloomlevel","modulid","punkte","aufgabetype"};
         try {
-            db.sqlInsert("nutzer", columns, values); // Beispiel für SQL Insert
-        } catch (SQLException e) {
-            e.printStackTrace();}
-        */
-
-        // test modul
-        /*try {
-            db.sqlInsert("modul",columns, values);
+            db.sqlInsert("nutzer",columns, values);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
         try {
-            db.sqlSelect("modul","name", "AIN4");
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }*/
-
-
-        //Benutzer Anlegen
-        /*
-        PreparedStatement ps;
-        try {
-            ps = db.getConn().prepareStatement("INSERT INTO benutzer (benutzername, passwort) VALUES ('User', '1234')");
-            int insertCount = ps.executeUpdate();
+            db.sqlDelete("nutzer","vorname", "netan");
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        */
 
-        benutzerKonto Nutzer = new benutzerKonto();
-        //Nutzer.fragenErstellen(1);
-        //Nutzer.fragenfiltern(1);
+
 
         //System.out.println(name);
-    }
-
-    @Override
-    public void start(Stage stage) throws Exception {
-        //Methode die GUI startet
-
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("/GUI/StartPage.fxml"));
-            Scene startScene = new Scene(root);
-            stage.setScene(startScene);
-            stage.show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
---
+++ src/main/resources/klausur_app_sql.sql
@@ -0,0 +1,85 @@
+CREATE TABLE benutzer (
+ id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+ benutzername varchar(55) NOT NULL,
+ passwort varchar(255) NOT NULL
+);
+
+CREATE TABLE modul (
+ id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+ name varchar(255) NOT NULL
+);
+
+CREATE TABLE klausur (
+ id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+ name VARCHAR(255) NOT NULL,
+ datum DATE NOT NULL,
+ gesamtpunkte DECIMAL(5,1) NOT NULL,
+ benutzer_id INT NOT NULL,
+ FOREIGN KEY (benutzer_id) REFERENCES benutzer(id)
+);
+
+CREATE TYPE taxonomie_stufe AS ENUM (
+ 'Erinnern',
+ 'Verstehen',
+ 'Anwenden',
+ 'Analysieren',
+ 'Bewerten',
+ 'Erschaffen'
+);
+
+CREATE TABLE aufgabe (
+ id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+ name VARCHAR(255) NOT NULL,
+ aufgabentext TEXT NOT NULL,
+ zeit INTERVAL NOT NULL,
+ format VARCHAR(20) NOT NULL,
+ punkte DECIMAL(4,1) NOT NULL,
+ taxonomie taxonomie_stufe,
+ benutzer_id INT NOT NULL,
+ FOREIGN KEY (benutzer_id) REFERENCES benutzer(id)
+);
+
+CREATE TABLE offene_aufgabe (
+ musterloesung TEXT NOT NULL,
+ aufgabe_id INT PRIMARY KEY,
+ FOREIGN KEY (aufgabe_id) REFERENCES aufgabe(id)
+);
+
+CREATE TYPE aufgaben_typ AS ENUM (
+ 'Single-Choice',
+ 'Multiple-Choice',
+ 'Wahr/Falsch',
+ 'Lückentext',
+ 'Zuordnung',
+ 'Ranking'
+);
+
+CREATE TABLE geschlossene_aufgabe (
+ typ aufgaben_typ NOT NULL,
+ aufgabe_id INT PRIMARY KEY,
+ FOREIGN KEY (aufgabe_id) REFERENCES aufgabe(id)
+);
+
+CREATE TABLE antwortmoeglichkeit_geschlossen (
+ id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+ antworttext TEXT NOT NULL,
+ ist_korrekt BOOLEAN NOT NULL,
+ geschlossene_aufgabe_id INT NOT NULL,
+ FOREIGN KEY (geschlossene_aufgabe_id) REFERENCES geschlossene_aufgabe(aufgabe_id)
+);
+
+CREATE TABLE aufgaben_klausur (
+ PRIMARY KEY (aufgabe_id, klausur_id),
+ aufgabe_id INT NOT NULL,
+ klausur_id INT NOT NULL,
+ FOREIGN KEY (aufgabe_id) REFERENCES aufgabe(id),
+ FOREIGN KEY (klausur_id) REFERENCES klausur(id)
+);
+
+CREATE TABLE aufgaben_modul (
+ PRIMARY KEY (aufgabe_id, modul_id),
+ aufgabe_id INT NOT NULL,
+ modul_id INT NOT NULL,
+ FOREIGN KEY (aufgabe_id) REFERENCES aufgabe(id),
+ FOREIGN KEY (modul_id) REFERENCES modul(id)
+);
